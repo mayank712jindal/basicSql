b28be7c074c67ce3ba25c9ab382f3a134cac0088
Mayank Jindal <mayank712jindal@gmail.com>
2021-07-12 00:50:48 +0530

    css

--- index.php
+++ index.php
@@ -8,16 +8,17 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="./style.css" />
     </head>
     <body>
-
-        <div class="container-fluid justify-content-center row">
-                <div class="col-12 text-center">
+        
+             <div class="col-12 text-center">
                     <h1>Job Registration</h1>
                 </div>
+        <div class="container-fluid justify-content-around row main">
+                
                 <div class="col-md-6">
-                    bgbfgng
+                    <img src="./undraw_Resume_re_hkth.svg">
                 </div>
                 <div class="col-md-6 text-center">
                     <form action="" method="POST" class="form-group">
